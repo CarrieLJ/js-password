ff4ababa988bebfb20828344e3fcabd529550b6d
Mona Lisa <cjensen@netechnical.com>
2022-11-09 12:59:56 -0600

    started if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,8 @@
-//make popup for characters count
-//answer popup
-//do again for special characters, numbers, lowercase, uppercase
-//create random password using selected criteria
-
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//moved this up so it happens right away
 generateBtn.addEventListener("click", writePassword);
+//variables for password creation
 var specChar = "!@#$%^&*()"
 var characters = "0123456789abcdefghijklmnopqrstuvwxyx!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   // var passwordLength = "";
@@ -22,19 +17,37 @@ function writePassword() {
 }
 
 function generatePassword() {
+  //variables with prompts and confirms for popups with text
   var length = window.prompt("How many characters do you want your password to contain?");
   var specChar1 = window.confirm("Do you want to include special characters?");
   var numbers = window.confirm("Do you want to include numbers?");
   var lowerCase = window.confirm("Do you want to include lowercase letters?");
   var upperCase = window.confirm("Do you want to include uppercase letters?");
   
-
+//variables for minimum
   var minLength = "";
   var minSpecChar1 = "";
   var minNumbers = "";
   var minLowerCase = "";
   var minUpperCase = "";
+
+  //what the user selected
+  if (length === )
+  if (specChar1 === True);
+  if (numbers === Ture);
+  if (lowerCase === True);
+  if (upperCase === True);
+
+  //
+  var passwordRules = {
+
+  }
 }
+
+
+
+
+
 
 
 
