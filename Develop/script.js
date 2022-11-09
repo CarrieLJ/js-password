9c2370c814dff01bdc4ede6ec95b3f484789cedd
Mona Lisa <cjensen@netechnical.com>
2022-11-09 11:43:10 -0600

    added variables to js

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,43 @@
+//make popup for characters count
+//answer popup
+//do again for special characters, numbers, lowercase, uppercase
+//create random password using selected criteria
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
+var specChar = "!@#$%^&*()"
+var characters = "0123456789abcdefghijklmnopqrstuvwxyx!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // var passwordLength = "";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword()
+  var passwordText = document.querySelector("#password")
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  var length = window.prompt("How many characters do you want your password to contain?");
+  var specChar1 = window.confirm("Do you want to include special characters?");
+  var numbers = window.confirm("Do you want to include numbers?");
+  var lowerCase = window.confirm("Do you want to include lowercase letters?");
+  var upperCase = window.confirm("Do you want to include uppercase letters?");
+  
+
+  var minLength = "";
+  var minSpecChar1 = "";
+  var minNumbers = "";
+  var minLowerCase = "";
+  var minUpperCase = "";
+}
+
+
+
+
+
+
